07723539dce4742fb702d0f9a0b146984d142041
Joshua Turner <joshturner1707@yahoo.co.uk>
2025-02-19 14:21:30 +0000

    Updating Main - Adding toFront() & setResize(false)

--- src/main/java/com/example/itticketsystem/Main.java
+++ src/main/java/com/example/itticketsystem/Main.java
@@ -11,11 +11,13 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("dashboard.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 650, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 750, 400);
         scene.getStylesheets().add(getClass().getResource("/com/example/itticketsystem/style.css").toExternalForm());
 
         stage.setTitle("IT TICKET SYSTEM");
         stage.setScene(scene);
+        stage.toFront();            // Automatically pops up
+        stage.setResizable(false);  // Stops resizing
         stage.show();
     }
 
