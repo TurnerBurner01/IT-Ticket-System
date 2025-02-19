d12fef6feb17bf683386f0de0976ca8bf0d3922d
Joshua Turner <joshturner1707@yahoo.co.uk>
2025-02-19 11:45:55 +0000

    Inital commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.itticketsystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.itticketsystem to javafx.fxml;
+    exports com.example.itticketsystem;
+}
