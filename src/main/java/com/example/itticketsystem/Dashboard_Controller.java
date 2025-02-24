0f154230eda9474ee1ae297a0b76d9a8e06afd72
Joshua Turner <joshturner1707@yahoo.co.uk>
2025-02-24 13:45:01 +0000

    Adding openDeleteTicketWindow()

--- src/main/java/com/example/itticketsystem/Dashboard_Controller.java
+++ src/main/java/com/example/itticketsystem/Dashboard_Controller.java
@@ -18,6 +18,7 @@ public class Dashboard_Controller {
     // Initialise Table Columns
     @FXML private TableView<Ticket> ticketTable;
     @FXML private TableColumn<Ticket, String> priorityColumn;
+    @FXML private TableColumn<Ticket, String> idColumn;
     @FXML private TableColumn<Ticket, Boolean> statusColumn;
     @FXML private TableColumn<Ticket, String> typeColumn;
     @FXML private TableColumn<Ticket, String> descriptionColumn;
@@ -32,6 +33,7 @@ public class Dashboard_Controller {
 
         // Set up table columns
         priorityColumn.setCellValueFactory(new PropertyValueFactory<>("priority"));
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
         statusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
         typeColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
         descriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
@@ -43,11 +45,11 @@ public class Dashboard_Controller {
 
     private void loadTestData() {
         // Adding test data to BinarySearchTree
-        ticketService.insert(new Ticket(0, true, "Critical Issue", "Floor Shut Down", "James W", "6/03/2025"));
-        ticketService.insert(new Ticket(0, true, "System Failure", "Urgent fix needed", "Alice W", "18/02/2025"));
-        ticketService.insert(new Ticket(0, true, "New Equipment", "Need a new monitor", "Bob J", "19/02/2025"));
-        ticketService.insert(new Ticket(0, true, "Critical Issue", "Server down", "Charlie Z", "17/02/2025"));
-        ticketService.insert(new Ticket(0, false, "Software Bug", "Photoshop not working", "David L", "20/02/2025"));
+        ticketService.insert(new Ticket(0, 0, true, "Critical Issue", "Floor Shut Down", "James W", "6/03/2025"));
+        ticketService.insert(new Ticket(0,0, true, "System Failure", "Urgent fix needed", "Alice W", "18/02/2025"));
+        ticketService.insert(new Ticket(0,0, true, "New Equipment", "Need a new monitor", "Bob J", "19/02/2025"));
+        ticketService.insert(new Ticket(0,0, true, "Critical Issue", "Server down", "Charlie Z", "17/02/2025"));
+        ticketService.insert(new Ticket(0,0,  false, "Software Bug", "Photoshop not working", "David L", "20/02/2025"));
 
         // Updating priorities
         ticketService.updatePriorities();
@@ -71,6 +73,29 @@ public class Dashboard_Controller {
             // Show the AddTicket window
             Stage stage = new Stage();
             stage.setTitle("Add New Ticket");
+            stage.setScene(new Scene(root));
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML private void openDeleteTicketWindow() {
+        try {
+            // Load the "Delete Ticket" window
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("delete-ticket.fxml"));
+            Parent root = fxmlLoader.load();
+
+            // Get the controller of the DeleteTicket window
+            DeleteTicket_Controller DeleteTickerController = fxmlLoader.getController();
+
+            // Pass the reference of this (Dashboard_Controller) to the DeleteTicket_Controller
+            DeleteTickerController.setDashboardController(this);
+
+            // Show the DeleteTicket window
+            Stage stage = new Stage();
+            stage.setTitle("Delete Ticket");
             stage.setScene(new Scene(root));
             stage.setResizable(false);
             stage.show();
