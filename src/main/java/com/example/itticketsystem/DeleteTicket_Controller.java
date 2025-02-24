42391ba0c90b56647c9450644a7ce13b27b12b0a
Joshua Turner <joshturner1707@yahoo.co.uk>
2025-02-24 13:54:22 +0000

    Set up for delete controller

--- src/main/java/com/example/itticketsystem/DeleteTicket_Controller.java
+++ src/main/java/com/example/itticketsystem/DeleteTicket_Controller.java
@@ -1,2 +1,11 @@
-package com.example.itticketsystem;public class DeleteTicket_Controller {
+package com.example.itticketsystem;
+
+public class DeleteTicket_Controller {
+
+    private Dashboard_Controller dashboardController;  // Reference to parent controller
+
+    // Set the Dashboard_Controller reference
+    public void setDashboardController(Dashboard_Controller dashboardController) {
+        this.dashboardController = dashboardController;
+    }
 }
