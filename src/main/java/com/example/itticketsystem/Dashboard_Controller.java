9c9ccc36995e0e0eeb73fcbdc4d774b93b4e9eb0
Joshua Turner <joshturner1707@yahoo.co.uk>
2025-03-12 16:16:39 +0000

    Added in refresh prioirty

--- src/main/java/com/example/itticketsystem/Dashboard_Controller.java
+++ src/main/java/com/example/itticketsystem/Dashboard_Controller.java
@@ -162,6 +162,9 @@ public class Dashboard_Controller {
             }
         }
 
+        // Update the Priority
+        ticketService.updatePriorities();
+
         // Refresh the table
         refreshTable();
     }
