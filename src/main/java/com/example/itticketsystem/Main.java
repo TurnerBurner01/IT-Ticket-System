5c4c133be2a9bd63e2ea314ef777369cf7c60637
Joshua Turner <joshturner1707@yahoo.co.uk>
2025-03-12 16:28:08 +0000

    Changing window size to fit the table

--- src/main/java/com/example/itticketsystem/Main.java
+++ src/main/java/com/example/itticketsystem/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("dashboard.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 685, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 740, 400);
         scene.getStylesheets().add(getClass().getResource("/com/example/itticketsystem/style.css").toExternalForm());
 
         stage.setTitle("IT TICKET SYSTEM");
